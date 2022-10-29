e2fc70b16775e87c9b378b6559e5d82d5e01741f
Donghan Kim <donghankim14@gmail.com>
2022-10-29 00:32:37 -0400

    final touches added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,39 @@
 class HomeController < ApplicationController
   def index
+    @moviesEmpty = false
+    @gamesEmpty = false
+    @podcastsEmpty = false
+    @showsEmpty = false
+
     if user_signed_in?
       @userMovies = Movie.where("user_id =?", current_user.id)
       @userGames = Game.where("user_id =?", current_user.id)
       @userPodcasts = Podcast.where("user_id =?", current_user.id)
       @userShows = TvShow.where("user_id =?", current_user.id)
+
+      @moviesEmpty = isEmpty(@userMovies)
+      @gamesEmpty = isEmpty(@userGames)
+      @podcastsEmpty = isEmpty(@userPodcasts)
+      @showsEmpty = isEmpty(@userShows)
     end
   end
+
+  def isEmpty(record)
+    return record.empty?
+  end
+
+  def friends
+    if !user_signed_in?
+      flash[:notice] = "You need to log in first!"
+      redirect_to new_user_session_path
+    end
+  end
+
+  def recommendations
+    if !user_signed_in?
+      flash[:notice] = "You need to log in first!"
+      redirect_to new_user_session_path
+    end
+  end
+
 end
