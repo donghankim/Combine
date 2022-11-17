8733d0c83bc27da4b56c3ee67d67217597d0490d
Donghan Kim <donghankim14@gmail.com>
2022-11-17 02:04:17 -0500

    please work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,21 +24,21 @@ class HomeController < ApplicationController
       @showsEmpty = isEmpty(@userShows)
 
       # process any search queries
-      # unless params[:query].nil?
-      #   api_req = fetch_movie(params[:query])
-      #   if api_req.key?("Error")
-      #     flash[:notice] = "Could not find \"" + params[:query] + "\" in IMDB database..."
-      #   else
-      #     @mediaData = []
-      #     api_req['Search'].each do |data|
-      #       id_ = data["imdbID"]
-      #       imdbRes = fetch_imdb(id_)
-      #       if (not imdbRes.key?("Error"))
-      #         @mediaData.append(imdbRes)
-      #       end
-      #     end
-      #   end
-      # end
+      unless params[:query].nil?
+        api_req = fetch_movie(params[:query])
+        if api_req.key?("Error")
+          flash[:notice] = "Could not find \"" + params[:query] + "\" in IMDB database..."
+        else
+          @mediaData = []
+          api_req['Search'].each do |data|
+            id_ = data["imdbID"]
+            imdbRes = fetch_imdb(id_)
+            if (not imdbRes.key?("Error"))
+              @mediaData.append(imdbRes)
+            end
+          end
+        end
+      end
     end
   end
 
@@ -85,36 +85,36 @@ class HomeController < ApplicationController
   end
 
   private
-    # # for rapidAPI request
-    # def fetch_movie(name)
-    #   endpoint = ERB::Util.url_encode(name)
-    #   url = URI("https://movie-database-alternative.p.rapidapi.com/?s=" + endpoint + "&r=json&page=1")
-    #   http = Net::HTTP.new(url.host, url.port)
-    #   http.use_ssl = true
-    #   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # for rapidAPI request
+    def fetch_movie(name)
+      endpoint = ERB::Util.url_encode(name)
+      url = URI("https://movie-database-alternative.p.rapidapi.com/?s=" + endpoint + "&r=json&page=1")
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    #   request = Net::HTTP::Get.new(url)
-    #   request["X-RapidAPI-Key"] = '5e123f2c8dmshc41d6e06564d61dp107f8djsn8f0e356490a4'
-    #   request["X-RapidAPI-Host"] = 'movie-database-alternative.p.rapidapi.com'
+      request = Net::HTTP::Get.new(url)
+      request["X-RapidAPI-Key"] = '5e123f2c8dmshc41d6e06564d61dp107f8djsn8f0e356490a4'
+      request["X-RapidAPI-Host"] = 'movie-database-alternative.p.rapidapi.com'
 
-    #   response = http.request(request)
-    #   return JSON.parse(response.body)
-    # end
+      response = http.request(request)
+      return JSON.parse(response.body)
+    end
 
-    # # fetch IMDB
-    # def fetch_imdb(imdbID)
-    #   endpoint = ERB::Util.url_encode(imdbID)
-    #   url = URI("https://movie-database-alternative.p.rapidapi.com/?r=json&i=" + endpoint)
-    #   http = Net::HTTP.new(url.host, url.port)
-    #   http.use_ssl = true
-    #   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # fetch IMDB
+    def fetch_imdb(imdbID)
+      endpoint = ERB::Util.url_encode(imdbID)
+      url = URI("https://movie-database-alternative.p.rapidapi.com/?r=json&i=" + endpoint)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    #   request = Net::HTTP::Get.new(url)
-    #   request["X-RapidAPI-Key"] = '5e123f2c8dmshc41d6e06564d61dp107f8djsn8f0e356490a4'
-    #   request["X-RapidAPI-Host"] = 'movie-database-alternative.p.rapidapi.com'
+      request = Net::HTTP::Get.new(url)
+      request["X-RapidAPI-Key"] = '5e123f2c8dmshc41d6e06564d61dp107f8djsn8f0e356490a4'
+      request["X-RapidAPI-Host"] = 'movie-database-alternative.p.rapidapi.com'
 
-    #   response = http.request(request)
-    #   return JSON.parse(response.body)
-    # end
+      response = http.request(request)
+      return JSON.parse(response.body)
+    end
 
 end
