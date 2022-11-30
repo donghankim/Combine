5d2b3c6a6d26127d8ff68c992ca393965803b98b
jade-i-k <55996281+jade-i-k@users.noreply.github.com>
2022-11-29 20:43:30 -0500

    Movie recommendations work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -82,6 +82,76 @@ class HomeController < ApplicationController
       flash[:notice] = "You need to log in first!"
       redirect_to new_user_session_path
     end
+
+    @friendsEmpty = false
+    if user_signed_in?
+      @userFriends = Friend.where("user_id =?", current_user.id)
+
+      @friendsEmpty = isEmpty(@userFriends)
+      if @friendsEmpty == false
+        puts("STARTINGGGGGgg")
+        #keep track of how many times each genre is applicable
+        @genres = Hash.new(0)
+        #go through each friend
+        @userFriends.each do |friend|
+          puts("LOOKING AT FRIENDS")
+          puts(friend.name)
+          #go through each piece of media
+          Movie.where("user_id =?", friend.name).each do |movie|
+            puts("LOOKING AT MOVIES")
+            puts(movie.name)
+            @genresArray = movie.genre.split
+            #add each genre to the hash
+            @genresArray.each do |genre|
+              #ensure there are no commas
+              genre.delete! ','
+              puts("NEW GENRE BEING ADDED")
+              puts(genre)
+              @genres[genre] = @genres[genre] + 1
+              puts("THE GENRE NOW HAS THIS MANY ENTRIES")
+              puts @genres[genre]
+            end
+          end
+        end
+        #the hash is created and the most popular genre stored below
+        @mostPopularGenre = @genres.max_by{|k,v| v}.first
+        puts("MOST POP GENRE IS")
+        puts(@mostPopularGenre)
+          
+        #go through every friend and make list of every item seen with that genre
+        @applicableMedia = Array.new
+
+        @userFriends.each do |friend|
+          Movie.where("user_id =?", friend.name).each do |movie|
+            @genresArray = movie.genre.split
+            @genresArray.each do |genre|
+              genre.delete! ','
+              if genre == @mostPopularGenre
+                @applicableMedia.push movie
+                puts("THIS WAS A MATCH")
+                puts movie.name
+              end
+            end
+          end
+        end
+        #applicableMedia should be filled in
+        puts("WE ARE CHOOSING FROM")
+        puts @applicableMedia
+
+        @recommendation = @applicableMedia.sample
+        puts("WE CHOSE")
+        puts @recommendation.name
+
+        #who has seen this and is friends with the user
+        @recommendedBy = Array.new
+        Movie.where("name =?", @recommendation.name).each do |hasSeen|
+          @poss = User.where("id =?", hasSeen.user_id).first
+          if Friend.where("name =?", @poss.id).where("user_id =?", current_user.id).present?
+            @recommendedBy.push @poss
+          end
+        end
+      end
+    end
   end
 
   private
